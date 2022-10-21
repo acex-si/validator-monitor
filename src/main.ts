aa59ecd5c2a4b3793ac02f32a2c0544649fb81ae
Marko Boben <marko.boben@gmail.com>
2022-10-21 13:30:32 +0200

    Added graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { exit } from 'process';
 import { AppModule } from './module';
 import { WinstonLogger } from './services/logger';
 
@@ -20,8 +21,18 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api-docs', app, document);
 
+    const startGracefulShutdown = async () => {
+        Logger.log('Starting shutdown of server');
+        await app.close();
+        Logger.log('Server shut down');
+        exit(1);
+    }
+    process.on('SIGTERM', startGracefulShutdown);
+    process.on('SIGINT', startGracefulShutdown);
+
     const configService = app.get(ConfigService);
     const port = configService.get<number>('PORT') || 8501;
+
     await app.listen(port, () => {
         Logger.log(`Server is listening on port ${port}`);
     });
